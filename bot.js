1858a3041d149c080c0b25b7e16f65a2986a1c76
jasonstatham777 <jason.statham.story@gmail.com>
2025-09-20 13:27:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,37 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // --- ВАШИ ДАННЫЕ ---
-// Токен теперь берется из безопасного хранилища на сервере, а не из кода
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const gameShortName = 'cherkashidzerun'; // <-- ВАШЕ КОРОТКОЕ ИМЯ ИГРЫ ИЗ ШАГА 2
-const gameUrl = 'https://jasonstatham777.github.io/cherkashidze-game/'; // <-- ВАША ССЫЛКА НА ИГРУ ИЗ ШАГА 1
+const gameShortName = 'cherkashidzerun'; // <-- Ваше короткое имя
+const gameUrl = 'https://jasonstatham777.github.io/cherkashidze-game/'; // <-- Ваша ссылка на игру
 // --------------------
-
-// Проверка, что токен был предоставлен
-if (!token) {
-  console.error('Ошибка: TELEGRAM_BOT_TOKEN не найден! Убедитесь, что вы добавили его в Environment Variables на Render.');
-  process.exit(1);
-}
 
 const bot = new TelegramBot(token, { polling: true });
 
+// Эта функция теперь будет обрабатывать команды /start и /game
+const sendGame = (chatId) => {
+  bot.sendGame(chatId, gameShortName);
+};
+
+// Отвечаем на команды в личных сообщениях и группах
 bot.onText(/\/start/, (msg) => {
-  bot.sendGame(msg.chat.id, gameShortName);
+  sendGame(msg.chat.id);
 });
 
-bot.on('inline_query', (query) => {
-  bot.answerInlineQuery(query.id, [{
-    type: 'game',
-    id: '1', // Просто уникальный ID для этого ответа
-    game_short_name: gameShortName
-  }]);
+bot.onText(/\/game/, (msg) => {
+  sendGame(msg.chat.id);
 });
 
+// Этот обработчик для нажатия на кнопку "Play"
 bot.on('callback_query', function onCallbackQuery(callbackQuery) {
   bot.answerCallbackQuery(callbackQuery.id, { url: gameUrl });
 });
 
-
 console.log(`Бот для игры "${gameShortName}" успешно запущен!`);
 
-
+// --- Часть кода, чтобы бот не "засыпал" ---
+const http = require('http');
+http.createServer(function (req, res) {
+  res.write("I'm alive");
+  res.end();
+}).listen(8080);
